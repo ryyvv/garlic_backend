8ca427c3163d5360c8bc83ecfe441f962b4cbbf8
ryy <ryjpascual.gates@gmail.com>
2025-11-15 13:19:58 +0800

    updated the user model

--- app/models/plant_location.py
+++ app/models/plant_location.py
@@ -5,32 +5,34 @@ import uuid
 import os
 
 class PlantLocationBase(SQLModel):
-    reg_name: str
-    reg_code: str
-    prov_name: str
-    prov_code: str
-    mun_name: str
-    mun_code: str
-    brgy_name: str
-    brgy_code: str
-    lat: float
-    long: float
+    region: str
+    region_code: str
+    province: str
+    province_code: str
+    city: str
+    city_code: str
+    barangay: str
+    barangay_code: str
+    latitude: float
+    longitude: float
+    user_id: Optional[uuid.UUID] = None
 
 class PlantLocation(PlantLocationBase, table=True):
     __tablename__ = "plant_location"
     __table_args__ = {"schema": os.getenv("POSTGRES_SCHEMA")}
 
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True, index=True)
-    reg_name: str = Field(max_length=255)
-    reg_code: str = Field(max_length=255)
-    prov_name: str = Field(max_length=255)
-    prov_code: str = Field(max_length=255)
-    mun_name: str = Field(max_length=255)
-    mun_code: str = Field(max_length=255)
-    brgy_name: str = Field(max_length=255)
-    brgy_code: str = Field(max_length=255)
-    lat: float
-    long: float
+    region: str = Field(max_length=255)
+    region_code: str = Field(max_length=255)
+    province: str = Field(max_length=255)
+    province_code: str = Field(max_length=255)
+    city: str = Field(max_length=255)
+    city_code: str = Field(max_length=255)
+    barangay: str = Field(max_length=255)
+    barangay_code: str = Field(max_length=255)
+    latitude: float
+    longitude: float
+    user_id: Optional[uuid.UUID] = Field(nullable=True)
 
 class PlantLocationCreate(PlantLocationBase):
     pass
@@ -39,13 +41,14 @@ class PlantLocationRead(PlantLocationBase):
     id: uuid.UUID
 
 class PlantLocationUpdate(SQLModel):
-    reg_name: Optional[str] = None
-    reg_code: Optional[str] = None
-    prov_name: Optional[str] = None
-    prov_code: Optional[str] = None
-    mun_name: Optional[str] = None
-    mun_code: Optional[str] = None
-    brgy_name: Optional[str] = None
-    brgy_code: Optional[str] = None
-    lat: Optional[float] = None
-    long: Optional[float] = None
+    region: Optional[str] = None
+    region_code: Optional[str] = None
+    province: Optional[str] = None
+    province_code: Optional[str] = None
+    city: Optional[str] = None
+    city_code: Optional[str] = None
+    barangay: Optional[str] = None
+    barangay_code: Optional[str] = None
+    latitude: Optional[float] = None
+    longitude: Optional[float] = None
+    user_id: Optional[uuid.UUID] = None
